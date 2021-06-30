c292b9b5768667b617d7325c530011d7bffba6b6
osalysdecatarina <72741076+osalysdecatarina@users.noreply.github.com>
2021-06-30 08:28:25 +0200

    ajouts

--- Account.cs
+++ Account.cs
@@ -4,38 +4,44 @@ using System.Text;
 
 namespace ConsoleApp5
 {
-    class Compte
+    public class Account
     {
         //Attributs
         public string name;
-        private int number;
+        private string number;
         private double balance;
         public double uncovered;
 
         //Accesseurs / Modifieurs
         double Balance { get => balance; set => balance = value; }
-        int Number { get => number; set => number = value; }
+        string Number { get => number; set => number = value; }
 
         //Constructeurs
-        public Compte(){
+        public Account(){
             name = "Compte indéfini";
-            number = 000;
+            number = "000";
             balance = 0;
             uncovered = -100;
         }
-        public Compte(string _name, int _number, double _balance, double _uncovered){
+        public Account(string _name, string _number, double _balance, double _uncovered){
             name = _name;
             number = _number;
             Balance = _balance;
             uncovered = _uncovered;
         }
         //méthodes
-        public string showInformations(){
+        public string getNumber
+        {
+            get
+            {
+                return number;
+            }
+        }
+        public string  showInformations(){
             return "Nom: " + name + "\nNuméro de compte: " + number + "\nSolde: " + balance + "\nDecouvert Autorisé" + uncovered;
         }
         public void credit(int amount){
             this.balance += amount;
-            Console.WriteLine("Solde actuel: " + Balance + " euros.");
         }
         public bool debite(int amount){
             if (balance - amount >= uncovered){
@@ -47,27 +53,25 @@ namespace ConsoleApp5
                 return false;
             }
         }
-        public bool transfer(int amount, Compte thisOne){
+        public bool transfer(int amount, Account thisOne){
             bool enough;
             if (Balance - amount >= uncovered){
                 this.debite(amount);
                 thisOne.credit(amount);
-                Console.WriteLine("Argent transféré");
                 return true;
             }
             else{
                 return false;
             }
         }
-        public bool compare(Compte thisOne) {
+        public bool compare(Account thisOne) {
             if (this.Balance > thisOne.Balance){
-                Console.WriteLine("Le compte " + this.name + " est supérieur au compte :" + thisOne.name);
                 return true;
             }
             else {
-                Console.WriteLine("Le compte " + thisOne.name + " est supérieur au compte :" + thisOne.name);
                 return false;
             }
         }
+   
     }
 }
---
+++ Bank.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public class Bank
+    {
+        //Attribut(s)
+        public Account[] myAccounts;
+        public int nbAccounts;
+        public string name;
+        public string city;
+
+        //Constructeur(s)
+        public Bank(string _name, string _city){
+            this.city = _city;
+            this.name = _name;
+            myAccounts = new Account[20];
+            this.nbAccounts = 0;
+        }
+        //Méthode(s)
+        private void addOne(Account _a1){
+            this.myAccounts[nbAccounts] = _a1;
+            nbAccounts++;
+        }
+        public void inIt(){
+            Account c1= new Account("Bertrand","123456", -3000, 4500.50);
+            Account c2 = new Account("Hoflack","223456", -200, 1500);
+            Account c3 = new Account("Dupont","32456", -500, 1500);
+            this.addOne(c1);
+            this.addOne(c2);
+            this.addOne(c3);
+        }
+        public override string ToString()
+        {
+            string accounts = "Les différents comptes de la banque " + this.name + " basée à " + city + "sont: ";
+            for(int i=0; i < this.nbAccounts; i++){
+                accounts += this.myAccounts[i].ToString() + "\n";
+            }
+            return accounts;
+        }
+        public Account returnAccount(string _accNumber){
+            for(int i=0; i < this.nbAccounts; i++){
+                if (_accNumber == myAccounts[i].getNumber){
+                    return myAccounts[i];
+                }
+            }
+            return null;
+        }
+        public bool transfer(string accdeb, string accben, double amount){
+            if (returnAccount(accdeb) != null&& returnAccount(accben) != null){
+                if (returnAccount(accdeb)).debite(amount) == true){
+
+                }
+            }
+        }
+    }
+}
